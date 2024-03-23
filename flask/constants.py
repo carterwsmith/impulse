ca0dffce83b3b358906ad4d632c0c90ebee10956
carterwsmith <csmithonthetrack@gmail.com>
2024-03-23 15:08:45 -0500

    pas

--- flask/constants.py
+++ flask/constants.py
@@ -1,4 +1,4 @@
-# Define the number of minutes that should pas before a session is not considered active.
+# Define the number of minutes that should pass before a session is not considered active.
 ACTIVE_SESSION_TIMEOUT_MINUTES = 1
 
 # Define the number of seconds that should pass before generating prompts for active sessions.
