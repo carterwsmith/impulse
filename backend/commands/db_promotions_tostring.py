34d1e7a1b02cac5a4edddde779f4c4fb1112b1bf
carterwsmith <csmithonthetrack@gmail.com>
2024-04-05 13:17:04 -0500

    basic promotion recommendation via browser alert

---
+++ backend/commands/db_promotions_tostring.py
@@ -0,0 +1,42 @@
+import sqlite3
+
+# From a session UUID, extract the user and all active promotions to construct a string suitable for an LLM prompt.
+def promotions_tostring(session_id, test=False):
+    # Connect to the SQLite database
+    conn = sqlite3.connect('backend/db/app.db')
+    cursor = conn.cursor()
+
+    cursor.execute('SELECT django_user_id FROM app_sessions WHERE id = ?', (session_id,))
+    django_user_id = cursor.fetchone()[0]
+
+    cursor.execute('SELECT * FROM app_promotions WHERE django_user_id = ?', (django_user_id,))
+    promotions = cursor.fetchall()
+
+    conn.close()
+
+    promotion_string = ""
+    for promotion_tuple in promotions:
+        promotion_string += "<promotion>\n"
+        is_ai_generated = promotion_tuple[11]
+        if is_ai_generated:
+            ai_columns = promotion_tuple[6:11]
+            promotion_string += f"Description of this promotion's target audience: {ai_columns[0]}\n"
+            if ai_columns[1]: # this means the range is in dollars
+                promotion_string += f"Range of possible discount in USD: from {ai_columns[2]} to {ai_columns[1]}\n"
+            else: # range in percent
+                promotion_string += f"Range of possible discount in percent off: from {ai_columns[4]} to {ai_columns[3]}\n"
+        else:
+            nonai_columns = promotion_tuple[1:5] + tuple([promotion_tuple[12]])
+            promotion_string += f"Title of the promotion: {nonai_columns[4]}\n"
+            promotion_string += f"Description of the promotion: {nonai_columns[1]}\n"
+            if nonai_columns[3]: # dollars
+                promotion_string += f"Discount in USD: {nonai_columns[3]}\n"
+            else:
+                promotion_string += f"Discount in percent off: {nonai_columns[2]}\n"
+        promotion_string += "</promotion>\n"
+
+    if test:
+        print(promotion_string)
+        return
+    else:
+        return promotion_string
--- backend/utils.py
+++ backend/utils.py
@@ -5,6 +5,7 @@ import requests
 import anthropic
 from dotenv import load_dotenv
 
+from commands.db_promotions_tostring import promotions_tostring
 from commands.db_session_tostring import session_tostring
 
 load_dotenv()
@@ -14,18 +15,25 @@ def _anthropic_client():
     client = anthropic.Anthropic()
     return client
 
-def session_to_prompt(session_id, directive=None):
+def session_to_prompt(session_id, directive=None, test=False):
     session_str = session_tostring(session_id).strip()
+    promotion_str = promotions_tostring(session_id).strip()
 
-    prompt_str = f'''You will be given the browsing and mouse position data (recorded each second) from the user of a website.\nIn the <t> tag will be the text of the element they are hovering over at that time, or the tag of the element itself if there is no text.\n\n<browsing_data>\n{session_str}\n</browsing_data>'''
+    prompt_str = f'''You will be given the browsing and mouse position data (recorded each second) from the user of a website.\nIn the <t> tag will be the text of the element they are hovering over at that time, or the tag of the element itself if there is no text.\n\n<browsing_data>\n{session_str}\n</browsing_data>\n\n'''
+    prompt_str += f'''You will now be given a list of possible promotions from the creator of the website. Some will have specific discount parameters and others will have discount ranges.\n\n{promotion_str}'''
     if directive:
         prompt_str += f"\n\n{directive}"
 
-    return prompt_str
+    if test:
+        print(prompt_str)
+        return
+    else:
+        return prompt_str
 
-DEFAULT_DIRECTIVE = "In under 140 characters, summarize what the user is trying to do on this website. Use as much of the data as possible to make your decision. Bias towards the LATEST POSSIBLE data because it is the most recent."
+SUMMARY_DIRECTIVE = "In under 140 characters, summarize what the user is trying to do on this website. Use as much of the data as possible to make your decision. Bias towards the LATEST POSSIBLE data because it is the most recent."
+PROMOTION_DIRECTIVE = "Use all of the available browsing data, with a bias towards the LATEST POSSIBLE because it is the most recent. If there is a promotion that fits the user's browsing activity, output that promotion's title (if provided) and description EXACTLY and nothing else. If there is a range provided, select a discount within the range that you predict will be most effective for this specific user. Be very selective and only choose one if the user shows some intent that makes the promotion relevant for them. If not, output 'no promotion' and nothing else."
 DEFAULT_SYSTEM_PROMPT = "You are an expert in psychology and consumer behavior."
-def prompt_claude_session_context(session_id, directive=DEFAULT_DIRECTIVE):
+def prompt_claude_session_context(session_id, directive=PROMOTION_DIRECTIVE):
     client = _anthropic_client()
     prompt_contents = session_to_prompt(session_id, directive)
 
