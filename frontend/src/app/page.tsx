ca335954fe7aea814637ba16df483a4333c00a98
carterwsmith <csmithonthetrack@gmail.com>
2024-04-17 13:04:59 -0500

    basic auth with auth.js, resend

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,6 +1,16 @@
+import { auth } from "@/auth"
+import { redirect } from 'next/navigation'
+
+
 import Content from "@/components/content"
 
-export default function Home() {
+export default async function Home() {
+  const session = await auth()
+
+  if (!session) {
+    redirect('/api/auth/signin')
+  }
+
   return (
     <Content/>
   )
