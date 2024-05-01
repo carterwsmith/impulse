6313323551cf1f79c0cfcccdb662b9d715016b7b
carterwsmith <csmithonthetrack@gmail.com>
2024-05-01 17:50:17 -0500

    custom magic link signin email

--- frontend/src/auth.ts
+++ frontend/src/auth.ts
@@ -3,10 +3,42 @@ import PostgresAdapter from "@auth/pg-adapter"
 import Resend from "next-auth/providers/resend"
 import { Pool } from "pg"
 import type { Provider } from "next-auth/providers"
+import { render } from "@react-email/render"
+import LinkEmail from "@/emails/LinkEmail"
  
 const providers: Provider[] = [
   Resend({
     from: "auth@onpulsehq.com",
+    async sendVerificationRequest({
+      identifier: to,
+      provider,
+      url,
+      theme,
+    }) {
+      const { host } = new URL(url)
+      const res = await fetch("https://api.resend.com/emails", {
+        method: "POST",
+        headers: {
+          Authorization: `Bearer ${provider.apiKey}`,
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          from: "Onpulse <auth@onpulsehq.com>",
+          to,
+          subject: `Sign in to Onpulse`,
+          html: render(LinkEmail({ url, host })),
+          text: text({ url, host }),
+        }),
+      })
+
+      // Email Text body (fallback for email clients that don't render HTML, e.g. feature phones)
+      function text({ url, host }: { url: string; host: string }) {
+        return `Sign in to ${host}\n${url}\n\n`
+      }
+    
+      if (!res.ok)
+        throw new Error("Resend error: " + JSON.stringify(await res.json()))
+    },
   }),
 ]
 
